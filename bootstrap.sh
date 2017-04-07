cdb7bffb71abfbdb4048c2fba14eea648d6437d7
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-04-07 23:08:13 +0100

    Updated repos

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,10 @@ set +x
 echo
 echo "[Info] Bootstrap is cloning the repo"
 
-git clone --recursive --branch master http://git.drupal.org/sandbox/marcelovani/2210081.git drupal_demo
+git clone --recursive --branch master https://github.com/marcelovani/drupal_demo.git drupal_demo
 
 echo "[Info] Bootstrap has finished installation."
 echo
 echo To install Distro run 
-echo $ cd drupal_demo
-echo $ ./install.sh
+echo cd drupal_demo
+echo ./install.sh [destination]
