c3e71d22b62254852d977f47eddf322fa0971dfa
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-04-08 00:37:03 +0100

    Fixed autobuild

--- scripts/autobuild.sh
+++ scripts/autobuild.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set +x
 
-sh scripts/bootstrap.sh
+echo
+echo "[Info] Bootstrap is cloning the repo"
+
+git clone --recursive --branch 7.x https://github.com/marcelovani/drupal_demo.git drupal_demo
+cd drupal_demo
 sh scripts/build.sh
