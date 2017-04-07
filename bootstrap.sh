7f213851550860ef046414af790cb35563bcf8d6
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-04-07 23:16:03 +0100

    Changed branch

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ set +x
 echo
 echo "[Info] Bootstrap is cloning the repo"
 
-git clone --recursive --branch master https://github.com/marcelovani/drupal_demo.git drupal_demo
+git clone --recursive --branch 7.x https://github.com/marcelovani/drupal_demo.git drupal_demo
 
 echo "[Info] Bootstrap has finished installation."
 echo
