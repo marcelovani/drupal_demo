02df61473edf2edcefcceb21464ca4fab9762a5c
Marcelo <marcellovani@yahoo.co.uk>
2017-04-19 11:10:51 +0100

    Enable modules

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,8 @@ set +x
 function f_install_site() {
   echo "[INFO] Installing Drupal"
   /usr/local/drush/drush si -y drupal_demo --db-url="mysql://root@localhost/drupal_demo" --site-name="Drupal demo" --account-name=admin --account-pass=demo --sites-subdir=default
+  # Enable modules
+  /usr/local/drush/drush en -y site_core, simpletest
 }
 
 f_install_site
