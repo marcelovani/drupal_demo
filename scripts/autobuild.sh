6de183412a8feddc2f8fb1045efe9776c9bea53a
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-04-22 14:18:30 +0100

    Using composer to build

--- scripts/autobuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set +x
-
-echo
-echo "[Info] Bootstrap is cloning the repo"
-
-git clone --recursive --branch 7.x https://github.com/marcelovani/drupal_demo.git drupal_demo
-cd drupal_demo
-sh scripts/build.sh
