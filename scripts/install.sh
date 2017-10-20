b7f866e02df9f1bfebdc4c59b10728d1da7dad1b
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-10-20 13:37:30 +0100

    Enable modules

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,9 +13,13 @@ function f_install_site() {
   --sites-subdir=default
 
   # Enable modules
-  drush en -y simpletest
-  drush dis -y toolbar
-  drush dis -y overlay
+  drush en -y simpletest, sharerich, captcha_keypad, blocks_mass_cache, \
+  cache_purger, context_admin_exclude, admin_menu, admin_devel, \
+  admin_menu_toolbar, module_filter, metatag, context, ctools, \
+  entity, features, googleanalytics, nodequeue, pathauto, strongarm, \
+  token, views_content, views_ui, wysiwyg, xmlsitemap, libraries
+
+  drush dis -y toolbar, overlay
 }
 
 f_install_site
