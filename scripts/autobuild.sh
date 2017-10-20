9fbf7ddc4b5bb3326e182be40d1348924d5ce30e
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-10-20 11:56:05 +0100

    Fixing build, using sqlite

--- scripts/autobuild.sh
+++ scripts/autobuild.sh
@@ -7,3 +7,5 @@ echo "[Info] Bootstrap is cloning the repo"
 git clone --recursive --branch 7.x https://github.com/marcelovani/drupal_demo.git drupal_demo
 cd drupal_demo
 sh scripts/build.sh
+php -S 127.0.0.1:8008
+
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,9 +3,10 @@ set +x
 
 function f_install_site() {
   echo "[INFO] Installing Drupal"
-  /usr/local/drush/drush si -y drupal_demo --db-url="mysql://root@localhost/drupal_demo" --site-name="Drupal demo" --account-name=admin --account-pass=demo --sites-subdir=default
+  drush si -y drupal_demo --db-url=sqlite://sites/default/files/.ht.sqlite --account-name=admin --account-pass=demo --sites-subdir=default
+
   # Enable modules
-  /usr/local/drush/drush en -y site_core, simpletest
+  drush en -y site_core, simpletest
 }
 
 f_install_site
